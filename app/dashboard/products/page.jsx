478cf6529d8765c042c46cc93add7bb36c9ae08a
DhruvilSheth15 <129719296+DhruvilSheth15@users.noreply.github.com>
2024-01-09 19:25:13 +0530

    Static UI done
    Need to understand "usePathname" from "next/navigation"

---
+++ app/dashboard/products/page.jsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+
+const ProductsPage = () => {
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={"/noproduct.jpg"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  Phone
+                </div>
+              </td>
+              <td>This is a good product</td>
+              <td>$100</td>
+              <td>Dummy</td>
+              <td>2</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/products/%7D`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form >
+                    <input type="hidden" name="id" />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
+
+export default ProductsPage;
